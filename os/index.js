05921c374756085ee5cab635b1926039fda36936
ronakdadwani <dadwanironak456@gmail.com>
2024-11-20 12:52:01 +0530

    OS Operating system module

---
+++ os/index.js
@@ -0,0 +1,25 @@
+const { log } = require('console');
+const os = require('os')
+
+// console.log('CPU ARCHITECTURE : ' + os.arch());
+
+// console.log("Free Memory > " , os.freemem());
+
+// console.log("Total Memory : " , os.totalmem());
+
+// // console.log("OS Interface : " , JSON.stringify(os.networkInterfaces()));
+
+// console.log("OS default dir :" , os.tmpdir());
+
+
+// console.log("Endianess :" + os.endianness());
+
+// console.log("Hostname :" + os.hostname());
+
+// console.log("OS type : " + os.type());
+
+// console.log("OS Platform : " + os.platform());
+
+// console.log("OS Release : " + os.release());
+
+    
